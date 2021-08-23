de8a99a62a862b836e4972736209b922e23a366a
Kati Sarajärvi <kati.sarajarvi@iki.fi>
2021-08-23 14:55:04 +0300

    updated kirjailijat.sql

--- kirjailijat.sql
+++ kirjailijat.sql
@@ -1,6 +1,7 @@
-create table Алексей Константинович Толстой (
+create table Алексей_Константинович_Толстой (
     id, integer primary key, 
     venäjäksi text, 
     suomeksi text, 
     englanniksi text, 
+    tyyppi text
     luettu integer);
